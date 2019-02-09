b6cf05780b15eb040701801d46423cec0649570e
Pranav Jain <pranajain@gmail.com>
2019-02-09 13:40:27 -0500

    Adding documentation on this code block below.

--- src/server/lib/asyncMiddleware.js
+++ src/server/lib/asyncMiddleware.js
@@ -1,8 +1,14 @@
+/** https://medium.com/@Abazhenov/using-async-await-in-express-with-node-8-b8af872c0016
+ * When using async/await with express endpoints, errors could be handled improperly and we
+ * need to design a try/catch block around each function.
+ * Alex Bazhenov wrote this medium article about how to solve that problem while keeping your code clean.
+ * I've linked the article above and used a version of his code below.
+ **/
 const asyncUtil = fn =>
   function asyncUtilWrap(...args) {
-    const fnReturn = fn(...args)
-    const next = args[args.length-1]
+    const fnReturn = fn(...args);
+    const next = args[args.length-1];
     return Promise.resolve(fnReturn).catch(next)
-  }
+  };
 
 module.exports = asyncUtil;
